97447c34caede6733fc9284778a73467be203898
Danny Le <dannylewastaken@protonmail.com>
2024-01-10 22:39:31 -0500

    Make instance.rs ref counted

--- src/abstraction/instance.rs
+++ src/abstraction/instance.rs
@@ -1,11 +1,16 @@
 // Contains information regarding the Vulkan instance
 
+use std::sync::Arc;
 use ash::vk;
+
+struct InstanceInner {
+    handle: ash::Instance,
+    entry: ash::Entry,
+}
 
 #[derive(Clone)]
 pub struct Instance {
-    pub handle: ash::Instance,
-    pub entry: ash::Entry,
+    handle: Arc<InstanceInner>,
 }
 
 pub struct InstanceCreateInfo {
@@ -14,5 +19,27 @@ pub struct InstanceCreateInfo {
 }
 
 impl Instance {
-    pub fn new(entry: ash::Entry, required_extensions: Vec<String>) {}
+    pub fn new(handle: ash::Instance, entry: ash::Entry) -> Self {
+        Instance {
+            handle: Arc::new(
+                InstanceInner { handle, entry }
+            )
+        }
+    }
+
+    pub fn get_vk_instance(&self) -> &ash::Instance {
+        &self.handle.handle
+    }
+
+    pub fn get_vk_entry(&self) -> &ash::Entry {
+        &self.handle.entry
+    }
+}
+
+impl Drop for InstanceInner {
+    fn drop(&mut self) {
+        unsafe {
+            self.handle.destroy_instance(None);
+        }
+    }
 }
